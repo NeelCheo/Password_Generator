f103ec3021aecc26de30e6a3233f0ded2ede0eaf
NeelCheo <73446815+NeelCheo@users.noreply.github.com>
2022-12-12 21:06:42 -0500

    added the required prompts and created the generate password function

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// var alphabet = ['abcdefghijklmnopqrstuvwxyz']
+// var c = alphabet.split('')
+// console.log(c)
+
+function generatePassword() {
+  pLength = window.prompt("What length is password (1-128)")
+  pLower = window.prompt("Is lowercase required (Y/N)")
+  pUpper = window.prompt("Is uppercase required (Y/N)")
+  pNumeric = window.prompt("Is Numeric Required (Y/N)")
+  pSpecial = window.prompt("Is special Required (Y/N)")
+}
+
+
+
+
+
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
